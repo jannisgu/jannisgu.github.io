f1a4f9cdf4233ff9eb536614cd390d49f384d15f
Jannis Günther <guenther.jannis@gmail.com>
2023-10-20 18:04:56 +0200

    phone adaption to header img and content width

--- static/script.js
+++ static/script.js
@@ -1,4 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  animation_experience_lists();
+
+  const touch_bool = 'ontouchstart' in window ? true : false;
+  let phone_bool = window.innerWidth < 768 ? true : false;
+
+  if (phone_bool) {
+    const phone_img = document.querySelector(".phone-img");
+    phone_img.style.display = "block";
+    phone_img.style.left = '80%';
+    phone_img.style.scale = 1.1;
+
+    setTimeout(() => {
+      phone_img.style.scale = 1.0;
+    }, 1000);
+    setTimeout(() => {
+      phone_img.style.zIndex = '-2';
+    }, 2000);
+  }
+
+
+});
+
+
+
+
+function animation_experience_lists() {
   const experienceLists = document.querySelectorAll(".experience-list");
 
   experienceLists.forEach((list) => {
@@ -18,4 +45,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
   });
-});
+}
